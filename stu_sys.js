7b05c58b4822c3a0edffc152b936fe42b38801af
godning3 <736397792@qq.com>
2017-08-23 00:36:16 +0800

    update addStudent showStudent

--- stu_sys.js
+++ stu_sys.js
@@ -22,6 +22,28 @@ function check_not(str) {
     return false;
 
 }
+function build_grade(str_list, grade) {
+    for (let i = 3; i < str_list.length; i++) {
+        let arr = str_list[i].split(':');
+        let type = arr[0];
+        let num = arr[1];
+
+        if(type == "数学"){
+            grade.ma = Number(num);
+        }
+        if(type == "语文"){
+            grade.ch = Number(num);
+        }
+        if(type == "英语"){
+            grade.en = Number(num);
+        }
+        if(type == "编程"){
+            grade.pg = Number(num);
+        }
+    }
+    return grade;
+}
+
 function addStudent(stu_list) {
     let query = Std.question;
     let ques = "请输入学生信息（格式：姓名, 学号, 班级, 学科: 成绩, ...），按回车提交：";
@@ -34,7 +56,11 @@ function addStudent(stu_list) {
     let name = str_list[0];
     let id = str_list[1];
     let clazz = str_list[2];
-    let grade = new Grade(100,100,100,100);
+    let grade = new Grade(0, 0, 0, 0);
+    //李赵宁, 0101, 1, 英语:90, 语文:90, 数学:90, 编程:90
+    //李宁, 0102, 1, 英语:88, 语文:88, 数学:88, 编程:88
+    grade = build_grade(str_list, grade);
+    // let grade = new Grade(100,100,100,100);
     let student = new Student(name, id, clazz, grade);
     stu_list.push(student);
     console.log("学生"+student.name+"的成绩被添加");
@@ -42,21 +68,25 @@ function addStudent(stu_list) {
 }
 
 function build_stu_info(student) {
-    let str = `
-成绩单
-姓名|数学|语文|英语|编程|平均分|总分 
-========================
-`;
+
     let total = student.grade.total();
     let average = student.grade.average();
     let arr = [student.name, student.grade.ch, student.grade.ma, student.grade.en, student.grade.pg, total, average];
-    str += arr.join('|');
-    str += `
-========================
-全班总分平均数：xxx
-全班总分中位数：xxx
-`;
+    let str = arr.join('|');
+
     return str;
+}
+
+function build_class_average(stu_list) {
+    let total = 0;
+    for (let stu of stu_list) {
+        total += stu.grade.total();
+    }
+    return total / stu_list.length;
+}
+
+function build_class_mid(stu_list) {
+    return stu_list[Math.floor(stu_list.length / 2)].grade.total();
 }
 
 function show_student(stu_list) {
@@ -67,14 +97,26 @@ function show_student(stu_list) {
         ques = "请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：";
         str = query(ques);
     }
-    let stu_id_list = str.split(',');
-    stu_id_list.map(function (id) {
-       let student = stu_list.find( n => n.id == id);
-       if(student){
-           let print_stu_info = build_stu_info(student);
-           console.log(print_stu_info);
-       }
-    });
+    let stu_id_list = str.split(', ');
+    let print_str = "成绩单\n";
+    print_str += "姓名|数学|语文|英语|编程|平均分|总分 \n";
+    print_str += "========================\n";
+
+    for (let id of stu_id_list) {
+        let student = stu_list.find( n => n.id == id);
+        if(student){
+            print_str += build_stu_info(student);
+            print_str += "\n";
+        }
+    }
+
+    let class_average = build_class_average(stu_list);
+    let class_mid = build_class_mid(stu_list);
+
+    print_str += "========================\n";
+    print_str += "全班总分平均数："+class_average+"\n";
+    print_str += "全班总分中位数："+class_mid;
+    console.log(print_str);
 }
 
 function stu_sys() {
